7e5f63df02facd7358e30f3154b59227abfe658b
itsraulfraire <rfrairegaytan@gmail.com>
2025-09-10 14:40:18 -0500

    Update usuarios_service.py

--- services/usuarios_service.py
+++ services/usuarios_service.py
@@ -1,7 +1,7 @@
 from repositories.usuarios_repository import autenticar_usuario
 
-def iniciar_sesion(usuario, contrasena):
-    user = autenticar_usuario(usuario, contrasena)
+def iniciar_sesion(nombre_usuario, contrasena):
+    user = autenticar_usuario(nombre_usuario, contrasena)
     print("DEBUG >> Resultado de query:", user)
     if user is not None:
         return {"status": "ok", "user": user}
