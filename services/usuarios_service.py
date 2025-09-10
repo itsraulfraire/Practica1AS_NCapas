e0694eb93b9a6a9881d0871c4407aac5a09c6216
itsraulfraire <rfrairegaytan@gmail.com>
2025-09-10 14:18:00 -0500

    Update usuarios_service.py

--- services/usuarios_service.py
+++ services/usuarios_service.py
@@ -2,7 +2,6 @@ from repositories.usuarios_repository import autenticar_usuario
 
 def iniciar_sesion(usuario, contrasena):
     user = autenticar_usuario(usuario, contrasena)
-    if user:
-        # Aquí podrías generar un token, guardar sesión, etc.
+    if user is not None:
         return {"status": "ok", "user": user}
     return {"status": "error", "message": "Usuario y/o Contraseña incorrectos"}
