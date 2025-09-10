98e2deea5c68fc0dc14cc460f768e83bb9163b8a
itsraulfraire <rfrairegaytan@gmail.com>
2025-09-10 14:34:28 -0500

    Update usuarios_service.py

--- services/usuarios_service.py
+++ services/usuarios_service.py
@@ -2,6 +2,8 @@ from repositories.usuarios_repository import autenticar_usuario
 
 def iniciar_sesion(usuario, contrasena):
     user = autenticar_usuario(usuario, contrasena)
+    print("DEBUG >> Resultado de query:", user)
     if user is not None:
         return {"status": "ok", "user": user}
     return {"status": "error", "message": "Usuario y/o Contraseña incorrectos"}
+
