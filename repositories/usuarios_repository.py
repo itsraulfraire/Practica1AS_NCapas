705788bcb05f2327020bf7b6ce492a49ebae9a38
itsraulfraire <rfrairegaytan@gmail.com>
2025-09-10 14:39:50 -0500

    Update usuarios_repository.py

--- repositories/usuarios_repository.py
+++ repositories/usuarios_repository.py
@@ -1,15 +1,17 @@
 from config.database import get_connection
 
-def autenticar_usuario(usuario, contrasena):
+def autenticar_usuario(nombre_usuario, contrasena):
     con = get_connection()
     cursor = con.cursor(dictionary=True)
     query = """
-        SELECT id_usuario, nombre_usuario
+        SELECT id_usuario
         FROM usuarios
-        WHERE nombre_usuario = %s AND contrasena = %s
+
+        WHERE nombre_usuario = %s
+        AND contrasena = %s
         LIMIT 1
     """
-    cursor.execute(query, (usuario, contrasena))
+    cursor.execute(query, (nombre_usuario, contrasena))
     result = cursor.fetchone()
     cursor.close()
     con.close()
